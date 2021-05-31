cbde45d4cd40a543f303288786a02f5f77fc7754
MarceloRicardoLopesJunior <84341271+MarceloRicardoLopesJunior@users.noreply.github.com>
2021-05-31 14:06:43 -0300

    Add files via upload

---
+++ charpter4.py
@@ -0,0 +1,50 @@
+teams=["psg","juventus","ajax","real madrid"]
+print(teams)
+teams[2]="manchester city"
+print(teams)
+teams.append("são paulo")
+print(teams)
+teams.insert(1,"palmeiras")
+print(teams)
+del teams[0]
+print(teams)
+del teams[3]
+print(teams)
+popped_teams = teams.pop()
+popped_teams = teams.pop(0)
+print(teams)
+print(popped_teams)
+teams.remove("juventus")
+print(teams)
+teams.remove("manchester city")
+print(teams)
+teams=["psg","juventus","ajax","real madrid"]
+#teams.sort()
+print(teams)
+numbers=[15,54,334,21]
+numbers.sort()
+print(numbers)
+sorted2=sorted(teams)
+print(sorted2)
+print(teams)
+cities=["campinas","ribeirão preto","foz do iguaçu","santa catarina"]
+print(cities)
+sorted_cities=sorted(cities)
+print(sorted_cities)
+print(cities)
+cities.sort()
+print(cities)
+cities.reverse()
+print(cities)
+print(len(cities)+1)
+print(cities[-1])
+print(cities[-2])
+print(cities[-5])
+
+
+
+
+
+
+
+
